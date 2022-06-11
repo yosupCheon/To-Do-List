3eb461b408b4dbe0c63eb214dd13b6c91b90b712
Yosup Cheon <yosupc@sfu.ca>
2022-06-11 10:38:55 -0700

    added remove button with some errors to be fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const init = () => {
     document.querySelector('form').addEventListener('submit', addList);
+    document.querySelector('ul').addEventListener('click', removeItemFromList);
 };
 
 const addList = (event) => {
@@ -8,10 +9,20 @@ const addList = (event) => {
     if (item.value !== ''){
         let list = document.querySelector('ul');
         let newItem = document.createElement('li');
-        newItem.innerHTML = `<label>${item.value}</label>`;
+        newItem.innerHTML = `<label>${item.value}</label> <input type="button" value = "remove"/>`;
         list.appendChild(newItem);
         item.value = '';
     };
+};
+
+// remove button works
+// problems
+// 1. adding list is limited to two (addList or html)
+// 2. button name "remove" overwrites on the listed item (addList.innterHTML)
+const removeItemFromList = () => {
+    let parent = document.querySelector('ul');
+    let child = document.querySelector('li');
+    let remove = parent.removeChild(child);
 };
 
 init();
