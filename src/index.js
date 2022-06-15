c3c59b13eb3b5dec493697b785c4fcfabb52fed6
Yosup Cheon <yosupc@sfu.ca>
2022-06-15 03:11:38 -0700

    fixed list nubmers and remove button

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const init = () => {
     document.querySelector('form').addEventListener('submit', addList);
-    document.querySelector('ul').addEventListener('click', removeItemFromList);
+    document.querySelector('ol').addEventListener('click', removeItemFromList);
 };
 
 const addList = (event) => {
     event.preventDefault();
     let item = document.querySelector('input');
     if (item.value !== ''){
-        let list = document.querySelector('ul');
+        let list = document.querySelector('ol');
         let newItem = document.createElement('li');
-        newItem.innerHTML = `<label>${item.value}</label> <input type="button" value = "remove"/>`;
+        //newItem.innerHTML = `<input type="button" value = "remove"/><label>${item.value}</label>`;
+        //newItem.innerHTML = `<label>${item.value}</label>`;
+        newItem.innerHTML = `<label>${item.value}</label><input type="checkbox"><input id ="remove" type="submit" value="remove">`;
         list.appendChild(newItem);
         item.value = '';
     };
@@ -17,12 +19,16 @@ const addList = (event) => {
 
 // remove button works
 // problems
-// 1. adding list is limited to two (addList or html)
-// 2. button name "remove" overwrites on the listed item (addList.innterHTML)
-const removeItemFromList = () => {
-    let parent = document.querySelector('ul');
-    let child = document.querySelector('li');
-    let remove = parent.removeChild(child);
+// 1. remove button always deletes the first list not the targeted one
+const removeItemFromList = (event) => {
+    //console.log("test\n");
+    if (event.target.id == "remove"){
+        console.log(`${event}`);
+        //let parent = document.querySelector('ol');
+        //let child = document.querySelector('li');
+        //parent.removeChild(child);
+        document.querySelector('ol').removeChild(document.querySelector('li'));
+    }
 };
 
 init();
