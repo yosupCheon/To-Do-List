5ec89ac63cee20934c5ee9bbcac2b9046fd5607a
Yosup Cheon <yosupc@sfu.ca>
2022-06-27 21:13:07 -0700

    added IPC main to renderer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
+/* for timer option has been taken out for save and load features
+newItem.innerHTML = `
+    <input type = "time">
+    <label>${item} <i>completed:</i></label><input type="checkbox"><input id ="remove" type="submit" value="remove">
+    `;
+*/
+
 const {ipcRenderer} = require('electron');
 const init = () => {
+    // this adds text to the app
+    // for better readability
+    // this needs to be function
+    let fs = require('fs');
+    fs.readFile('scheduler.txt', (err, data)=>{
+        if (err) {
+            console.error(err);
+            return;
+            }
+            let arr = data.toString().split("\n");
+            for(i in arr) {
+                if (arr[i] !== ''){
+                    let item = arr[i];
+                    let list = document.querySelector('ol');
+                    let newItem = document.createElement('li');
+                    newItem.innerHTML = `
+                    <label>${item} <i>completed:</i></label><input type="checkbox"><input id ="remove" type="submit" value="remove">
+                    `;
+                    list.appendChild(newItem);
+                    item = '';
+            }
+        }
+    });
+    
     document.querySelector('form').addEventListener('submit', addList);
     document.querySelector('ol').addEventListener('click', removeItemFromList);
 };
@@ -11,7 +42,6 @@ const addList = (event) => {
         let list = document.querySelector('ol');
         let newItem = document.createElement('li');
         newItem.innerHTML = `
-        <input type = "time">
         <label>${item.value} <i>completed:</i></label><input type="checkbox"><input id ="remove" type="submit" value="remove">
         `;
         ipcRenderer.send("item-from-renderer", item.value);
